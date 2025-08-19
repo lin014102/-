b1af3570b823f459016c0dbb598240dfb50b53d3
lin014102 <jiayu8227@gmail.com>
2025-08-19 14:50:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from flask import Flask, request, jsonify
 import os
 
 app = Flask(__name__)
-
-# LINE Bot 設定 (暫時用假的值)
-line_bot_api = LineBotApi('your_channel_access_token')
-handler = WebhookHandler('your_channel_secret')
 
 @app.route('/')
 def home():
@@ -19,24 +12,9 @@ def health():
     return {'status': 'ok'}
 
 @app.route("/webhook", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    
+def webhook():
+    # 暫時只是接收 webhook，不處理
     return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # 簡單的回音功能
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="你說：" + event.message.text)
-    )
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
